914b0e50471959de0d621528a15378659a632321
wangyw15 <wangyanwei2012@sina.com>
2023-04-20 13:17:52 +0800

    游戏后端框架

---
+++ RescueLimboBackend/Controllers/GameProgressStorage.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace RescueLimboBackend.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class GameProgressStorage : ControllerBase
+{
+    [HttpGet(Name = "increase")]
+    public void IncreaseCountByKey(string key)
+    {
+        Console.WriteLine($"{key} count has been increased.");
+        // [TODO)
+    }
+}
